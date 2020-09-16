7099000e7fcc577bb0cee7b4be15396061535db1
DanielFMC <daniel970508@gmail.com>
2020-09-16 16:21:45 -0500

    Diseño responsive

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const sharedButton = document.querySelector(".post--profile__shareButton");
 const popover = document.querySelector(".post--profile__popover");
+const info = document.querySelector(".post--profile__info");
+const mobile = window.matchMedia("screen and (max-width: 768px)");
 
-sharedButton.addEventListener("click", function () {
+function showPopover() {
   popover.classList.toggle("is-active");
-});
+}
+
+function hideInfo() {
+  info.classList.toggle("is-not-visible");
+  showPopover();
+}
+
+mobile.addListener(validation);
+
+function validation(event) {
+  if (event.matches) {
+    sharedButton.removeEventListener("click", showPopover);
+    sharedButton.addEventListener("click", hideInfo);
+  } else {
+    sharedButton.addEventListener("click", showPopover);
+    sharedButton.removeEventListener("click", hideInfo);
+  }
+}
+
+validation(mobile);
